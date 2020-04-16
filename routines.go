2b603c1292db6c15403d740efd9f6e008e4f90b3
tomoko.hirata <t0m0k05238188+mac@gmail.com>
2020-04-17 04:06:12 +0900

    Add route

--- routines.go
+++ routines.go
@@ -1,30 +1,117 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"os"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	log.Print("Hello world received a request.")
-	target := os.Getenv("TARGET")
-	if target == "" {
-		target = "World"
+func TopHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("TopHandler request.")
+	var tmpText = `
+{{define "top"}}
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>{{.Title}}</title>
+</head>
+<body>
+{{.Message}}
+</body>
+</html>
+{{end}}
+`
+	tmpl := template.Must(template.New("top").Parse(tmpText))
+
+	// TODO: 値を取ってきて一覧表示する
+
+	dat := struct {
+		Title   string
+		Message string
+	}{
+		Title:   "Test",
+		Message: "ほげほげほげほげ",
 	}
-	fmt.Fprintf(w, "Hello %s!\n", target)
+	if err := tmpl.ExecuteTemplate(w, "top", dat); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func PageHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("PageHandler request.")
+	var tmpText = `
+{{define "top"}}
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>{{.Title}}</title>
+</head>
+<body>
+{{.Message}}
+{{.ID}}
+</body>
+</html>
+{{end}}
+`
+
+	tmpl := template.Must(template.New("top").Parse(tmpText))
+	vars := mux.Vars(r)
+
+	// TODO: 値をとってきてテンプレートに入れる
+
+	dat := struct {
+		Title   string
+		Message string
+		ID      string
+	}{
+		Title:   "Test",
+		Message: "ほげほげほげほげ",
+		ID:      vars["id"],
+	}
+	if err := tmpl.ExecuteTemplate(w, "top", dat); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func CreateHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("CreateHandler request.")
+
+	// TODO 受け取った値を取得する
+	//err := r.ParseForm()
+	//if err != nil {
+	//	// Handle error
+	//}
+	//
+	//var person Person
+	//// POSTフォームで送信された変数をstructに変換する
+	//err = decoder.Decode(&person, r.PostForm)
+	//if err != nil {
+	//	// Handle error
+	//}
+
+	// TODO 値を登録する
+
+	// TODO topにリダイレクト(個人ページに戻す）
+	newURL := "https://" + r.Host
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("location", newURL)
+	w.WriteHeader(http.StatusMovedPermanently)
 }
 
 func main() {
 	log.Print("Hello world sample started.")
+	r := mux.NewRouter()
+	// GET
+	r.HandleFunc("/", TopHandler)
+	r.HandleFunc("/page/{id}", PageHandler)
+	//r.HandleFunc("/new", NewPageHandler)
 
-	http.HandleFunc("/", handler)
+	// POST
+	r.HandleFunc("/create", CreateHandler)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	http.ListenAndServe(":8080", r)
 }
