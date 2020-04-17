885d4080c336096232f553e751b94ec3a29cb118
tomoko.hirata <t0m0k05238188+mac@gmail.com>
2020-04-17 22:51:46 +0900

    Add post cloud firestore

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module share-routine
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.4
+require (
+	cloud.google.com/go/firestore v1.2.0 // indirect
+	firebase.google.com/go v3.12.0+incompatible
+	github.com/gorilla/mux v1.7.4
+	github.com/gorilla/schema v1.1.0
+)
--- routines.go
+++ routines.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"context"
 	"html/template"
 	"log"
 	"net/http"
 
+	firebase "firebase.google.com/go"
 	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
 )
 
-func TopHandler(w http.ResponseWriter, r *http.Request) {
-	log.Print("TopHandler request.")
-	var tmpText = `
+// HTML
+var topTmpl = `
 {{define "top"}}
 <!DOCTYPE html>
 <html lang="ja">
@@ -24,26 +26,48 @@ func TopHandler(w http.ResponseWriter, r *http.Request) {
 </html>
 {{end}}
 `
-	tmpl := template.Must(template.New("top").Parse(tmpText))
 
-	// TODO: 値を取ってきて一覧表示する
+var newPageTmpl = `
+{{define "newpage"}}
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>{{.Title}}</title>
+</head>
+<body>
+<form action="/create" method="post">
+    <div>
+        <label for="title">名前をつけてください</label>
+        <input type="text" name="title" id="title" placeholder="私のモーニングルーティン" required>
+    </div>
+    <div>
+        <label for="firstRoutine">あなたが起きて最初にすることは?</label>
+        <input type="text" name="firstRoutine" id="firstRoutine" placeholder="大きく背を伸びる" required>
+    </div>
+    <div>
+        <label for="secondRoutine">あなたが起きて２番目にすることは?</label>
+        <input type="text" name="secondRoutine" id="secondRoutine" placeholder="カーテンを開けて日光をあびる">
+    </div>
+    <div>
+        <label for="thirdRoutine">あなたが起きて３番目にすることは?</label>
+        <input type="text" name="thirdRoutine" id="thirdRoutine" placeholder="コーヒーを入れる">
+    </div>
+    <div>
+        <label for="message">みんなにメッセージをどうぞ</label>
+        <input type="text" name="message" id="message" placeholder="気持ちのいい朝を過ごしましょう！！" required>
+    </div>
+    <div>
+        <button>登録する</button>
+    </div>
+</form>
+</body>
+</html>
+{{end}}
+`
 
-	dat := struct {
-		Title   string
-		Message string
-	}{
-		Title:   "Test",
-		Message: "ほげほげほげほげ",
-	}
-	if err := tmpl.ExecuteTemplate(w, "top", dat); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func PageHandler(w http.ResponseWriter, r *http.Request) {
-	log.Print("PageHandler request.")
-	var tmpText = `
-{{define "top"}}
+var pageTmpl = `
+{{define "page"}}
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -58,7 +82,53 @@ func PageHandler(w http.ResponseWriter, r *http.Request) {
 {{end}}
 `
 
-	tmpl := template.Must(template.New("top").Parse(tmpText))
+// UserRoutine ... ルーティン
+type UserRoutine struct {
+	Title         string `firestore:"title"`
+	FirstRoutine  string `firestore:"first_routine"`
+	SecondRoutine string `firestore:"second_routine"`
+	ThirdRoutine  string `firestore:"third_routine"`
+	Message       string `firestore:"message"`
+	ImageURL      string `firestore:"image_url"`
+}
+
+func TopHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("TopHandler request.")
+
+	tmpl := template.Must(template.New("top").Parse(topTmpl))
+
+	// TODO: 値を取ってきて一覧表示する
+	dat := struct {
+		Title   string
+		Message string
+	}{
+		Title:   "Test",
+		Message: "ほげほげほげほげ",
+	}
+	if err := tmpl.ExecuteTemplate(w, "top", dat); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func NewPageHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("NewPageHandler request.")
+
+	tmpl := template.Must(template.New("newpage").Parse(newPageTmpl))
+	dat := struct {
+		Title string
+	}{
+		Title: "新規作成",
+	}
+
+	if err := tmpl.ExecuteTemplate(w, "newpage", dat); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func PageHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("PageHandler request.")
+
+	tmpl := template.Must(template.New("page").Parse(pageTmpl))
 	vars := mux.Vars(r)
 
 	// TODO: 値をとってきてテンプレートに入れる
@@ -72,7 +142,7 @@ func PageHandler(w http.ResponseWriter, r *http.Request) {
 		Message: "ほげほげほげほげ",
 		ID:      vars["id"],
 	}
-	if err := tmpl.ExecuteTemplate(w, "top", dat); err != nil {
+	if err := tmpl.ExecuteTemplate(w, "page", dat); err != nil {
 		log.Fatal(err)
 	}
 }
@@ -80,22 +150,42 @@ func PageHandler(w http.ResponseWriter, r *http.Request) {
 func CreateHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print("CreateHandler request.")
 
-	// TODO 受け取った値を取得する
-	//err := r.ParseForm()
-	//if err != nil {
-	//	// Handle error
-	//}
-	//
-	//var person Person
-	//// POSTフォームで送信された変数をstructに変換する
-	//err = decoder.Decode(&person, r.PostForm)
-	//if err != nil {
-	//	// Handle error
-	//}
+	// 受け取った値を取得する
+	err := r.ParseForm()
+	if err != nil {
+		log.Printf("Parse error : %v", err)
+	}
 
-	// TODO 値を登録する
+	var routine UserRoutine
+	var decoder = schema.NewDecoder()
+	err = decoder.Decode(&routine, r.PostForm)
+	if err != nil {
+		log.Printf("decode error : %v", err)
+	}
+	log.Println(routine)
 
-	// TODO topにリダイレクト(個人ページに戻す）
+	// 登録する
+	projectID := "share-my-routine"
+	ctx := context.Background()
+	conf := &firebase.Config{ProjectID: projectID}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Printf("firebase.NewApp error : %v", err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Printf("Firestore error : %v", err)
+	}
+	defer client.Close()
+
+	_, _, err = client.Collection("userRoutines").Add(ctx, routine)
+	if err != nil {
+		log.Printf("Add collection error : %v", err)
+		return
+	}
+
+	// topに戻す
 	newURL := "https://" + r.Host
 	w.Header().Set("Content-Type", "text/html")
 	w.Header().Set("location", newURL)
@@ -108,8 +198,7 @@ func main() {
 	// GET
 	r.HandleFunc("/", TopHandler)
 	r.HandleFunc("/page/{id}", PageHandler)
-	//r.HandleFunc("/new", NewPageHandler)
-
+	r.HandleFunc("/new", NewPageHandler)
 	// POST
 	r.HandleFunc("/create", CreateHandler)
 
